dfea03e5382b35cac522c8b19418609c620b4822
Emir Peskovic <me@emir.dk>
2021-09-14 09:07:54 +0200

    Added comments

--- Console/ConsoleEx.cs
+++ Console/ConsoleEx.cs
@@ -14,6 +14,15 @@ namespace MoreEverything.Console
                 { typeof(bool), _ => ConsoleParse.GetBool() },
             };
 
+        // TODO: Add IRestrictions
+        /// <summary>
+        /// Runs a loop until the input is valid, then returns said input
+        /// </summary>
+        /// <param name="message">Optional message to write before the input</param>
+        /// <param name="newLine">Should the input be on the same line as the optional message or on a new line</param>
+        /// <param name="length">Used for arrays; sets size of input</param>
+        /// <typeparam name="T">Desired type</typeparam>
+        /// <returns>Desired type</returns>
         public static T GetInput<T>(string message = null, bool newLine = true, int length = 0)
         {
             if (message != null)
